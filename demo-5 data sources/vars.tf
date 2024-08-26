1a56fda6e5da4e6a6a661d5f10c5f8ecb0128b49
ssrikanth98 <srikanthseepani@gmail.com>
2024-08-26 02:12:51 -0700

    demo5

---
+++ demo-5 data sources/vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-0b0ea68c435eb488d"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
