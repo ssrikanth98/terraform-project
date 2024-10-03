5c2804088e796b927ba8ae3778a0b45f874b6f8e
ssrikanth98 <srikanthseepani@gmail.com>
2024-10-03 06:40:02 -0700

    packer and docker

---
+++ docker-demo-2/vars.tf
@@ -0,0 +1,26 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "ECS_INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+
+variable "ECS_AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-0b0ea68c435eb488d"
+    us-west-2 = "ami-56ed4936"
+    eu-west-1 = "ami-c8337dbb"
+  }
+}
+
+# Full List: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
